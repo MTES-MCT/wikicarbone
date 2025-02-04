1fb9b07ec0c4dacb447a3bb61ea7d89622ebced1
Nicolas Perriault <nicolas@perriault.net>
2025-02-04 18:23:12 +0100

    fix: allow overriding spinning country.

--- tests/Data/Textile/SimulatorTest.elm
+++ tests/Data/Textile/SimulatorTest.elm
@@ -51,7 +51,7 @@ suite =
                 [ { tShirtCotonFrance
                     | countrySpinning = Nothing
                   }
-                    |> expectImpact db ecs 1313.7
+                    |> expectImpact db ecs 1346.21
                     |> asTest "should compute a simulation ecs impact"
                 , describe "disabled steps"
                     [ { tShirtCotonFrance | disabledSteps = [ Label.Ennobling ] }
